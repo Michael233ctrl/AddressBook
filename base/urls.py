539ee8e869b93a978d7b0eb6df32d5f61e670a10
Michael233ctrl <mishabmv2503@gmail.com>
2021-10-31 13:38:49 +0200

    Added search functionality

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.ContactList.as_view(), name='contacts'),
     path('add-contact/', views.add_contact, name='add_contact'),
     path('edit-contact/<int:pk>/', views.edit_contact, name='edit_contact'),
+    path('delete-contact/<int:pk>/', views.delete_contact, name='delete_contact'),
+    path('search-contact/', views.search_contact, name='search_contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib import messages
@@ -8,8 +9,13 @@ from .forms import ContactForm
 
 class ContactList(ListView):
     model = Contact
-    template_name = 'base/all-contacts.html'
+    template_name = 'base/contacts.html'
     context_object_name = 'contacts'
+
+
+def contact_list(request):
+    contact = Contact.objects.all()
+    return render(request, 'base/contacts.html', {'contacts': contact})
 
 
 def add_contact(request):
@@ -37,3 +43,24 @@ def edit_contact(request, pk):
             messages.error(request, 'Invalid form')
     form = ContactForm(instance=contact)
     return render(request, 'base/contact.html', {'form': form, 'action': action})
+
+
+def delete_contact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contacts')
+    return render(request, 'base/delete.html', {'item': contact})
+
+
+def search_contact(request):
+    if request.method == 'POST':
+        searched = request.POST.get('searched')
+        contact = Contact.objects.filter(
+            Q(first_name__contains=searched) | Q(last_name__contains=searched) | Q(address__contains=searched) |
+            Q(city__contains=searched) | Q(country__contains=searched) | Q(phone_number__contains=searched)
+        )
+
+        return render(request, 'base/search.html', {'searched': searched, 'contacts': contact})
+
+    return render(request, 'base/search.html', {})
